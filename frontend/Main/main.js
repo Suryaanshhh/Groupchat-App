449ed2cae73bb9356c0412b31200f050e6f727bb
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-05-13 19:06:21 +0530

    Realtime chat done

--- frontend/Main/main.js
+++ frontend/Main/main.js
@@ -2,18 +2,25 @@ const SendButton = document.getElementById("Send");
 
 window.addEventListener("DOMContentLoaded", function () {
   const token = localStorage.getItem("token");
-  axios.get("http://localhost:4000/get-message", {
-    headers: {
-      Authorisation: token,
-    },
-  }).then(response=>{
-    for(var i=0;i<response.data.messages.length;i++){
-        console.log(response.data.messages[i].content)
-    }
-    
-  }).catch(err=>{
-    console.log(err)
-  });
+ setInterval(()=>{
+    axios
+    .get("http://localhost:4000/get-message", {
+      headers: {
+        Authorisation: token,
+      },
+    })
+    .then((response) => {
+      const MessageDiv = document.getElementById("message");
+      MessageDiv.textContent="";
+      for (var i = 0; i < response.data.messages.length; i++) {
+        //console.log(response.data.messages[i].content);
+        ShowMessages(response.data.messages[i]);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  },1000)
 });
 
 SendButton.addEventListener("click", function () {
@@ -34,6 +41,9 @@ SendButton.addEventListener("click", function () {
     });
 });
 
-function ShowMessages(){
-    
+function ShowMessages(message) {
+  const MessageDiv = document.getElementById("message");
+  const Li=document.createElement('li');
+  Li.textContent=`${message.content}`
+ MessageDiv.appendChild(Li);
 }
