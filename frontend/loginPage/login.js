23cfa93b6b0a1e153909ea309a954c4d147f4c97
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-05-13 09:55:03 +0530

    loginPageCreated

---
+++ frontend/loginPage/login.js
@@ -0,0 +1,36 @@
+const SubmitBtn = document.getElementById("submit");
+SubmitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const login = {
+    email: email,
+    password: password,
+  };
+
+  axios
+    .post(`http://16.170.71.187:3000/login-user/${login.email}`, login)
+    .then((response) => {
+      console.log(response);
+      if(response.status == 201) {
+        alert("User Loggedin");
+        localStorage.setItem("token", response.data.token);
+        window.location.href = "../main/main.html";
+      }
+    })
+    .catch((err) => {
+      alert(err)
+      console.log(err);
+    });
+  console.log(login.email);
+  console.log(login.password);
+});
+
+const forgetBTN = document.getElementById("forgot-password");
+
+forgetBTN.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("test");
+  window.location.href = "./forget/forget.html";
+});
